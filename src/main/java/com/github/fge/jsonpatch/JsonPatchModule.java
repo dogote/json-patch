31558cfd567ec7bc8dd55fa045b1eab02e5380f5
Jason Chaffee <jason@gearlaunch.com>
2017-01-03 17:21:42 -0800

    added support for RemoveOptionalOperation

--- src/main/java/com/github/fge/jsonpatch/JsonPatchModule.java
+++ src/main/java/com/github/fge/jsonpatch/JsonPatchModule.java
@@ -6,6 +6,7 @@ import com.github.fge.jsonpatch.operation.AddOperation;
 import com.github.fge.jsonpatch.operation.CopyOperation;
 import com.github.fge.jsonpatch.operation.MoveOperation;
 import com.github.fge.jsonpatch.operation.RemoveOperation;
+import com.github.fge.jsonpatch.operation.RemoveOptionalOperation;
 import com.github.fge.jsonpatch.operation.ReplaceOperation;
 import com.github.fge.jsonpatch.operation.TestOperation;
 
@@ -24,6 +25,7 @@ public class JsonPatchModule extends SimpleModule {
                 new NamedType(CopyOperation.class, CopyOperation.OPERATION_NAME),
                 new NamedType(MoveOperation.class, MoveOperation.OPERATION_NAME),
                 new NamedType(RemoveOperation.class, RemoveOperation.OPERATION_NAME),
+                new NamedType(RemoveOptionalOperation.class, RemoveOptionalOperation.OPERATION_NAME),
                 new NamedType(ReplaceOperation.class, ReplaceOperation.OPERATION_NAME),
                 new NamedType(TestOperation.class, TestOperation.OPERATION_NAME)
         );
